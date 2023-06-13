87a5a218eea246ae241ab34a146205d042716f2b
Waqar <urspecial1one@gmail.com>
2023-06-14 00:01:07 +0500

    First Commit Classed Based

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Index from './src';
+
+const App = () => <Index />;
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ src/static/entries.js
@@ -0,0 +1,65 @@
+export const ENTRIES1 = [
+  {
+    title: 'Beautiful and dramatic Antelope Canyon',
+    subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+    illustration: 'https://placeimg.com/640/480/nature',
+  },
+  {
+    title: 'Earlier this morning, NYC',
+    subtitle: 'Lorem ipsum dolor sit amet',
+    illustration: 'https://placeimg.com/640/480/nature',
+  },
+  {
+    title: 'White Pocket Sunset',
+    subtitle: 'Lorem ipsum dolor sit amet et nuncat ',
+    illustration: 'https://placeimg.com/640/480/nature',
+  },
+  {
+    title: 'Acrocorinth, Greece',
+    subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+    illustration: 'https://placeimg.com/640/480/nature',
+  },
+  {
+    title: 'The lone tree, majestic landscape of New Zealand',
+    subtitle: 'Lorem ipsum dolor sit amet',
+    illustration: 'https://placeimg.com/640/480/nature',
+  },
+  {
+    title: 'Middle Earth, Germany',
+    subtitle: 'Lorem ipsum dolor sit amet',
+    illustration: 'https://placeimg.com/640/480/nature',
+  },
+];
+
+export const ENTRIES2 = [
+  {
+    title: 'Favourites landscapes 1',
+    subtitle: 'Lorem ipsum dolor sit amet',
+    illustration: 'https://placeimg.com/640/480/nature',
+  },
+  {
+    title: 'Favourites landscapes 2',
+    subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+    illustration: 'https://placeimg.com/640/480/nature',
+  },
+  {
+    title: 'Favourites landscapes 3',
+    subtitle: 'Lorem ipsum dolor sit amet et nuncat',
+    illustration: 'https://placeimg.com/640/480/nature',
+  },
+  {
+    title: 'Favourites landscapes 4',
+    subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+    illustration: 'https://placeimg.com/640/480/nature',
+  },
+  {
+    title: 'Favourites landscapes 5',
+    subtitle: 'Lorem ipsum dolor sit amet',
+    illustration: 'https://placeimg.com/640/480/nature',
+  },
+  {
+    title: 'Favourites landscapes 6',
+    subtitle: 'Lorem ipsum dolor sit amet et nuncat',
+    illustration: 'https://placeimg.com/640/480/nature',
+  },
+];
